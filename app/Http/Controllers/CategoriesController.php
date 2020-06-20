0bcc69ff7caaa92d2b57359813381b46479e362c
kezakimana <kezakimanachrist2@gmail.com>
2020-06-21 01:35:18 +0200

    creer une migration

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -24,11 +24,39 @@ class CategoriesController extends Controller{
      public function store(Request $request)
      {
          # code...
+
+        $request->validate([
+          'cat_name'=>'required'
+
+        ]);
+
          $category = new Category();
-         $category ->cat_name = $request->cat_name();
+         $category ->cat_name = $request->cat_name;
          $category->save();
          return redirect('categories');
      }
-
-
+     //depandence 
+     public function edit(Category $category)
+     {
+         # code...
+         $category= Category::find($category->id);
+         return view('categories/edit', [
+          'category'=>$category
+         ]);
+     }
+     public function update(Request $request,Category $category)
+     {
+         # code...
+         $category->cat_name = $request->cat_name;
+         $category->save();
+         return redirect('categories');
+     }
+     public function destroy(Category $category)
+     {
+         # code...
+         $category=Category::find($category->id);
+         $category->delete();
+         return redirect('categories');
+     }
+ 
 }
