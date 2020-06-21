164d5baa1f9e1096ebb385b19cc70031ed8b01f5
kezakimana <kezakimanachrist2@gmail.com>
2020-06-21 02:15:59 +0200

    creer une migation  products

--- database/migrations/2020_06_20_233432_create_products_table.php
+++ database/migrations/2020_06_20_233432_create_products_table.php
@@ -15,6 +15,14 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->String('product_name');
+            
+            $table->integer('unit_price');
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('cascade');
             $table->timestamps();
         });
     }
