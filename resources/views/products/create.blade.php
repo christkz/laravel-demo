2659d2b2eb61b3c10f29a9c5d25bd129fa2ae3be
kezakimana <kezakimanachrist2@gmail.com>
2020-07-22 20:03:44 +0200

    make form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -24,29 +24,46 @@
 				<h1 class="page-header">Add Products</h1>
 			</div>
         </div><!--/.row-->
-		<form role="form" action="{{url('Products')}}" method="post">
-         		@csrf
-								<div class="form-group">
-									<label for='cat_name'>Category Name</label>
-                                    <Select name="category_id" id="" class="forme-control"
-                                    class="@error('unit_price') is-danger @enderror">
-                                    <option value="">Select Category</option>
-                                    @foreach($categories as $category)
-                                    
-                                       
-                                    
-                                    </Select>
+		<form role="form" action="{{url('products')}}" method="post">
+		@csrf
+						<div class="form-group">
+							<label for='cat_name'>Category Name</label>
+							<Select name="category_id" id="" class="forme-control"
+									class="@error('unit_price') is-danger @enderror">
+									<option value="">Select category</option>
+										@foreach($categories as $category)
+										<option value="{{$category->id}}">{{$category->cat_name}}</option>
+										@endforeach
+										@error('category_id')
+									<div class="alert alert-danger">{{$message}}</div>
+									@enderror
+							
+							</Select>
+			<div class="form-group">
+                    <label for="">Product name</label>
+                    <input type="text" name="product_name" id="" class="form-control" 
+                    class="@error('product_name') is-danger @enderror" placeholder="" aria-describedby="helpId">
+                    @error('product_name')
+                    <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+			 </div>
+			 <div class="form-group">
+                    <label for="">Unit price</label>
+                    <input type="text" name="unit_price" id="" class="form-control" 
+                    class="@error('unit_price') is-danger @enderror" placeholder="" aria-describedby="helpId">
+                    @error('unit_price')
+                    <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div>
 									
 								</div>
 
 								<button type="submit" class="btn btn-primary">
 								<span class="glyphicon glyphicon-save"> </span>
-						
-								Enregistre</button>
+						            	</button>
+                                <button class="btn btn-default" type="reset">Reset</button>
 									
 </form>
 </body>
 </html>
-
-
 @endsection
